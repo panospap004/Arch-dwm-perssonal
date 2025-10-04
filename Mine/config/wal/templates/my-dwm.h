f30c8aa3079d5ecaa41a0f0c96fc2dc968dae500
panospap004 <pappanos004@gmail.com>
2025-10-04 20:56:23 +0300

    fixes

--- Mine/config/wal/templates/my-dwm.h
+++ Mine/config/wal/templates/my-dwm.h
@@ -1,13 +1,6 @@
-dwm.normbordercolor: {color0} 
+dwm.normbordercolor: {color0}
 dwm.normbgcolor: {color0}
 dwm.normfgcolor: {color4}
 dwm.selbordercolor: {color8}
 dwm.selbgcolor: {color4}
 dwm.selfgcolor: {color0}
-
-// dwm.normbordercolor: {color8} 
-// dwm.normbgcolor: color0}
-// dwm.normfgcolor: {color15}
-// dwm.selbordercolor: {color15}
-// dwm.selbgcolor: {color1}
-// dwm.selfgcolor: {color15}

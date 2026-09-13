3e6efe822d355d5bbdf7867c4cb5544023528694
pappanos <pappanos004@gmail.com>
2026-09-14 00:04:12 +0300

    added rmpc support info on how to install it inside rmpc config folder

---
+++ Mine/config/rmpcd/init.lua
@@ -0,0 +1,49 @@
+-- ##############################################################
+-- ##                                                          ##
+-- ## This is an example config file. Uncomment and customize  ##
+-- ## to your liking. Check out the documentation for more     ##
+-- ## information. https://rmpc.mierak.dev/rmpcd/              ##
+-- ##                                                          ##
+-- ##############################################################
+
+---@type Config
+local config = {
+    -- Point rmpcd to your mpd server
+    address = "127.0.0.1:6600",
+}
+
+-- Enable mpris support
+-- config.mpris = true
+
+-- Disable TCP keepalive on the MPD socket (enabled by default)
+-- config.enable_keepalive = false
+
+-- Automatically increment play count on song change
+-- rmpcd.install("#builtin.playcount")
+
+-- Install last fm scrobbling builtin
+-- For now you have to request an API key yourself due to LastFM's insane API
+-- design https://www.last.fm/api/account/create
+-- rmpcd.install("#builtin.lastfm"):setup({
+-- 	api_key = "<your api key>",
+-- 	shared_secret = "<your shared secret key>",
+-- 	update_now_playing = false,
+-- })
+
+-- Install notification on song change builtin
+-- rmpcd.install("#builtin.notify"):setup({
+-- 	debounce_delay = 1000,
+-- })
+
+-- Install the auto lyrics download builtin
+-- rmpcd.install("#builtin.lyrics"):setup({
+-- 	debounce_delay = 1000,
+-- })
+
+-- Install the last played time tracking plugin from git repo using the integrated plugin manager.
+-- rmpcd.install({ url = "https://github.com/rmpc-org/rmpcd-lastplayed.git" })
+
+rmpcd.install("#builtin.playcount")
+rmpcd.install({ url = "https://github.com/rmpc-org/rmpcd-lastplayed.git" })
+
+return config
